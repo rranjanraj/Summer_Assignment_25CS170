03bac3faa4d2b77343462200207ad0530a87a5d0
rranjanraj <rranjanraj2007@gmail.com>
2026-06-15 21:41:13 +0530

    Day9 Assignment is uploaded

---
+++ Day9/Repeated_character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    for(char i='A';i<='E';i++){
+        for(char j='A';j<=i;j++){
+            printf("%c",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
